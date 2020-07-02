9ec693fc6e097852c50f005aa57e2e47ba9f2981
Stella Cannefax <stella@unity3d.com>
2020-07-02 04:35:05 -0700

    add matrix4x4 stub

---
+++ CoreData/Matrices.cpp
@@ -0,0 +1,1 @@
+#include "Matrices.h"
---
+++ CoreData/Matrices.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <FloatVectors.h>
+
+namespace StarEngine
+{
+	struct Matrix4x4
+	{
+	public:
+		float4 GetColumn()
+		{
+		}
+		float4 GetRow()
+		{
+		}
+
+		float4 SetColumn()
+		{
+		}
+
+		float4 SetRow()
+		{
+		}
+
+	private:
+		float matrix[16];
+	};
+}
+
+
